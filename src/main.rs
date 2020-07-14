30d4ece2cea9a8af5d1a43c3908826f3ee6b0144
WilliamBirdsall <will@ignition72.com>
2020-07-14 17:00:51 -0400

    Add boilerplate file reading code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+fn main() { 
+    use std::env; 
+    use std::fs; 
+
+    let filename = "../quotes.txt";
+    let contents = fs::read_to_string(filename)
+        .expect("Something went wrong reading the file");
+
+    println!("{}", contents); 
 }
